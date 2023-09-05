f1186088dda7c8ae969837a8c4c16048ef2abdf8
Enkhzaya Munkh-Ochir <jini.zaya@gmail.com>
2023-09-04 22:58:17 -0500

    navbar customized

--- website/auth.py
+++ website/auth.py
@@ -24,7 +24,7 @@ def login():
                 flash('Incorrect password, try again', category='error')
         else:
             flash('Email does not exist', category='error')
-    return render_template('login.html')
+    return render_template('login.html', user=current_user)
 
 @auth.route('/logout')
 @login_required
@@ -61,4 +61,4 @@ def sign_up():
             flash('Account created', category='success')
             return redirect(url_for('views.home'))
 
-    return render_template("signup.html")
+    return render_template("signup.html", user=current_user)
